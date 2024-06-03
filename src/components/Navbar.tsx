0151861bc494b224e88257dc4d91936bb6efb3fb
Christian Nainggolan <christiannainggolan2755@gmail.com>
2024-06-03 17:04:03 +0700

    feat:add green color to navbar button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,39 +5,84 @@ import { ArrowRight } from 'lucide-react';
 
 const Navbar = () => {
   const user = undefined;
+
   const isAdmin = false;
+
   return (
-    <nav className="sticky z-[100] h-14 inset-0 top-0 w-full border-b border-gray-300 bg-white/75 backdrop-blur-lg transition-all">
+    <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
-        <div className="flex h-14 items-center justify-between border-b border-zinc-100 ">
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="flex z-40 font-semibold">
-            case <span className="text-green-600">cobra</span>
+            case<span className="text-green-600">cobra</span>
           </Link>
 
           <div className="h-full flex items-center space-x-4">
             {user ? (
               <>
-                <Link href="/api/auth/logout" className={buttonVariants({ variant: 'ghost', size: 'sm' })}>
-                  Sign Out
+                <Link
+                  href="/api/auth/logout"
+                  className={buttonVariants({
+                    size: 'sm',
+                    variant: 'ghost',
+                  })}
+                >
+                  Sign out
                 </Link>
                 {isAdmin ? (
-                  <Link href="/api/auth/logout" className={buttonVariants({ variant: 'ghost', size: 'sm' })}>
-                    Dashboard
+                  <Link
+                    href="/dashboard"
+                    className={buttonVariants({
+                      size: 'sm',
+                      variant: 'ghost',
+                    })}
+                  >
+                    Dashboard ✨
                   </Link>
                 ) : null}
-                <Link href="/configure/upload" className={buttonVariants({ variant: 'ghost', size: 'sm', className: 'hidden sm:flex items-center gap-1' })}>
-                  Create Case
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: 'sm',
+                    className: 'hidden sm:flex items-center gap-1',
+                  })}
+                >
+                  Create case
                   <ArrowRight className="ml-1.5 h-5 w-5" />
                 </Link>
               </>
             ) : (
               <>
-                <Link href="/api/auth/register" className={buttonVariants({ variant: 'ghost', size: 'sm' })}>
-                  Sign Up
+                <Link
+                  href="/api/auth/register"
+                  className={buttonVariants({
+                    size: 'sm',
+                    variant: 'ghost',
+                  })}
+                >
+                  Sign up
                 </Link>
 
-                <Link href="/api/auth/login" className={buttonVariants({ variant: 'ghost', size: 'sm', className: 'hidden sm:flex items-center gap-1' })}>
+                <Link
+                  href="/api/auth/login"
+                  className={buttonVariants({
+                    size: 'sm',
+                    variant: 'ghost',
+                  })}
+                >
                   Login
+                </Link>
+
+                <div className="h-8 w-px bg-zinc-200 hidden sm:block" />
+
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: 'sm',
+                    className: 'hidden sm:flex items-center gap-1',
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="ml-1.5 h-5 w-5" />
                 </Link>
               </>
             )}
